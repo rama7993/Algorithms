7f99fd612127277c6e586c1665a3d266cd69886d
rama7993 <61896721+rama7993@users.noreply.github.com>
2022-03-04 19:06:19 +0530

    Update coin change.py

--- coin change.py
+++ coin change.py
@@ -2,13 +2,13 @@ def count(S, m, n ):
     if (n == 0): 
         return 1
     if (n < 0): 
-        return 0; 
+        return 0 
   
     if (m <=0 and n >= 1): 
         return 0
   
    
-    return count( S, m - 1, n ) + count( S, m, n-S[m-1] ); 
+    return min(count( S, m - 1, n ) ,1+ count( S, m, n-S[m-1] ))
 
 arr = [1, 2, 3] 
 m = len(arr) 
